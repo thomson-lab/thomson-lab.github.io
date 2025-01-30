24aeb2124ce040311affb3b5a41540acc7750bdf
bobthomson <thomsonr@hawaii.edu>
2025-01-30 00:37:37 +0000

    Deploying to gh-pages from @ thomson-lab/thomson-lab.github.io@18039b4af1539de677a1fdd9f044ad30b38e2900 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,28 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/join/";
           },
-        },{id: "post-a-post-with-image-galleries",
+        },{id: "dropdown-publications",
+              title: "publications",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-projects",
+              title: "projects",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/blog/";
+              },
+            },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
