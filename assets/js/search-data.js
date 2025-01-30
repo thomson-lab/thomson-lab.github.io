d62c17e43e776924974ec6d8c11a80981fd393eb
bobthomson <thomsonr@hawaii.edu>
2025-01-30 01:43:18 +0000

    Deploying to gh-pages from @ thomson-lab/thomson-lab.github.io@3f07fd15fd897a2e44617b2ed06c00d6375f7957 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
